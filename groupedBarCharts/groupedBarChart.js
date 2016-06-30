33afbe0e6b62c0b860af86fb03ebaa5a01d7c152
Kristin Henry <kristin.henry@slalom.com>
2016-06-30 11:33:43 -0700

    drawing two grouped barcharts on one page

--- groupedBarCharts/groupedBarChart.js
+++ groupedBarCharts/groupedBarChart.js
@@ -8,10 +8,11 @@ function drawGroupedBarChart(dataFile, targ, grpHeader, yLabel, colorRange){
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
+  var groupGap = .4; //.2;  // original was .1
   var x0 = d3.scale.ordinal()
-      .rangeRoundBands([0, width], .1);
+      .rangeRoundBands([0, width], groupGap);
   
-  var x1 = d3.scale.ordinal();  /// used within each band of x0, for groups
+  var x1 = d3.scale.ordinal();  /// used within each step of x0, for groups
 
   var y = d3.scale.linear()
       .range([height, 0]);
@@ -43,11 +44,13 @@ function drawGroupedBarChart(dataFile, targ, grpHeader, yLabel, colorRange){
 
     data.forEach(function(d) {
       d.subgroups = subgroupNames.map(function(name) { return {name: name, value: +d[name]}; });
+      //console.log(d.subgroups);
     });
 
     x0.domain(data.map(function(d) { return d[grpHeader]; }));
     x1.domain(subgroupNames).rangeRoundBands([0, x0.rangeBand()]);
     y.domain([0, d3.max(data, function(d) { return d3.max(d.subgroups, function(d) { return d.value; }); })]);
+
 
     svg.append("g")
         .attr("class", "x axis")
@@ -68,7 +71,8 @@ function drawGroupedBarChart(dataFile, targ, grpHeader, yLabel, colorRange){
         .data(data)
       .enter().append("g")
         .attr("class", "group")
-        .attr("transform", function(d) { return "translate(" + x0(d[groupsHeader]) + ",0)"; });
+        // .attr("transform", function(d) { return "translate(" + x0(d[groupsHeader]) + ",0)"; });
+        .attr("transform", function(d) { return "translate(" + x0(d[grpHeader]) + ",0)"; });
 
     group.selectAll("rect")
         .data(function(d) { return d.subgroups; })
@@ -76,6 +80,7 @@ function drawGroupedBarChart(dataFile, targ, grpHeader, yLabel, colorRange){
         .attr("width", x1.rangeBand())
         .attr("x", function(d) { return x1(d.name); })
         .attr("y", function(d) { return y(d.value); })
+        //.attr("y", function(d) { return 0; })
         .attr("height", function(d) { return height - y(d.value); })
         .style("fill", function(d) { return color(d.name); });
 
